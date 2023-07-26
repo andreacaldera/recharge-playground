bd0d3ae2267a7c47ab5230a9fa0a106fbf822485
Andrea Caldera <andrea.caldera@gmail.com>
2023-07-26 08:31:34 +0100

    point for correct file on payment service config

--- apps/payment-service/src/serverless.ts
+++ apps/payment-service/src/serverless.ts
@@ -24,7 +24,7 @@ const serverlessConfiguration = <Serverless>{
   service: 'payment-service',
   functions: {
     hello: {
-      handler: 'src/handler.hello',
+      handler: 'src/main.hello',
     },
   },
 };
